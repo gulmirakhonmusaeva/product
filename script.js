faa1b92a8d31168d0d140191816746d465c93ab2
gulmirakhonmusaeva <gulmirakhonmusaeva@gmail.com>
2022-01-03 23:47:53 +0500

    Add html css and all images

---
+++ script.js
@@ -0,0 +1,38 @@
+//  const cardOneEl = document.querySelector('.card-1')
+//  const cardTwoEl = document.querySelector('.card-2')
+//  const cardThreeEl = document.querySelector('.card-3')
+//  const dotOne = document.querySelector('.dot-1')
+//  const dotTwo = document.querySelector('.dot-2')
+//  const dotThree = document.querySelector('.dot-3')
+
+
+//      window.addEventListener('mouseenter' , function(e) {
+//         console.log(e);
+//         if(e == 'card-1'){
+//             mouseenterFunc()
+//         } else if(e == cardTwoEl) {
+//             mouseenterFunc()
+//         }else if(e == cardThreeEl) {
+//             mouseenterFunc()
+//         } else {
+//             mouseout()
+//         }
+        
+//      })
+    
+// // function eventHandler(event) {
+// //     if (event.type == 'cardOneEl'){
+// //         console.log(event.name);
+// //     }
+// // }
+
+// function mouseenterFunc() {
+//     dotOne.classList.add('active')
+//     dotTwo.classList.add('active')
+//     dotThree.classList.add('active')
+// }
+//  function mouseout(){
+//     dotOne.classList.remove('active')
+//     dotTwo.classList.remove('active')
+//     dotThree.classList.remove('active')
+// }
